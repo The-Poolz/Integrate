6aeb5dc280dc53b2acfc8174f87a58ec7ef80a5c
Ashwin Arora <ashwinarora48@gmail.com>
2021-12-08 19:04:10 +0530

    rearranging test and removing migrations

--- bin/build.sh
+++ bin/build.sh
@@ -17,23 +17,23 @@ mkdir ./contracts/EnvelopToken
 cp -a ./node_modules/poolz-envelop-token/contracts/. ./contracts/EnvelopToken
 
 
-mkdir ./test/1_PoolzBack
-cp -a ./node_modules/poolz-back/test/General/. ./test/1_PoolzBack
+mkdir ./test/01_PoolzBack
+cp -a ./node_modules/poolz-back/test/General/. ./test/01_PoolzBack
 
-mkdir ./test/2_WhiteList
-cp -a ./node_modules/poolz-whitelist/test/. ./test/2_WhiteList
+mkdir ./test/02_WhiteList
+cp -a ./node_modules/poolz-whitelist/test/. ./test/02_WhiteList
 
-mkdir ./test/0_LockedDeal
-cp -a ./node_modules/poolz-locked-deal/test/. ./test/0_LockedDeal
+mkdir ./test/03_LockedDeal
+cp -a ./node_modules/poolz-locked-deal/test/. ./test/03_LockedDeal
 
-mkdir ./test/4_Benefit
-cp -a ./node_modules/poolz-benefit/test/. ./test/4_Benefit
+mkdir ./test/04_Benefit
+cp -a ./node_modules/poolz-benefit/test/. ./test/04_Benefit
 
-mkdir ./test/5_HodlersWhitelist
-cp -a ./node_modules/poolz-hodlers-whitelist/test/. ./test/5_HodlersWhitelist
+mkdir ./test/05_HodlersWhitelist
+cp -a ./node_modules/poolz-hodlers-whitelist/test/. ./test/05_HodlersWhitelist
 
-mkdir ./test/6_EnvelopToken
-cp -a ./node_modules/poolz-envelop-token/test/. ./test/6_EnvelopToken
+mkdir ./test/06_EnvelopToken
+cp -a ./node_modules/poolz-envelop-token/test/. ./test/06_EnvelopToken
 
 #temporary fix for naming error of TestToken in Benefit tests
 sed -i 's/"TestToken"/"Token"/g' test/4_Benefit/test.js
