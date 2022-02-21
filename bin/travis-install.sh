c8ade10f24aeb6250f158388c18f210455318efc
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-02-21 16:59:54 +0200

    changed truffle version

--- bin/travis-install.sh
+++ bin/travis-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 npm install
-npm install -g ganache truffle
+npm install -g ganache@5.4.30 truffle
 # ganache-cli has moved to ganache 
 # https://github.com/trufflesuite/ganache/discussions/1075#user-content-v7.0.0-alpha.0-breaking-changes
