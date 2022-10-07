467536cc87ea1032d262d73dc6fbfa0b91758006
Andrew Dmytrenko <dmitrenkoae3@gmail.com>
2022-10-07 19:37:39 +0300

    added MultiWithdraw tests

---
+++ contracts/MultiWithdraw/MultiManageable.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "poolz-helper-v2/contracts/GovManager.sol";
+
+contract MultiManageable is GovManager {
+    constructor() {
+        maxTransactionLimit = 500;
+    }
+
+    address public LockedDealAddress;
+    uint256 public maxTransactionLimit;
+
+    function setLockedDealAddress(address _LockedDealAddress)
+        public
+        onlyOwnerOrGov
+    {
+        require(
+            LockedDealAddress != _LockedDealAddress,
+            "Can't set the same LockedDeal address"
+        );
+        LockedDealAddress = _LockedDealAddress;
+    }
+
+    function setMaxTransactionLimit(uint256 _newLimit) external onlyOwnerOrGov {
+        maxTransactionLimit = _newLimit;
+    }
+}
